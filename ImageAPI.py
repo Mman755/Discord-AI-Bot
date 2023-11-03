aeab80a48d1c380eb30da9fa7b15c276e32a505c
mman755 <mman755@aucklanduni.ac.nz>
2023-11-04 01:23:50 +1300

    🐛 Fixed a very goofy bug with the image endpoint model specification which did not exist before

--- ImageAPI.py
+++ ImageAPI.py
@@ -25,22 +25,21 @@ def get_image(PROMPT):
         'prompt': PROMPT, # User prompt
         'n': 1, # Number of images to generate based on given prompt
         'size': '1024x1024', # Resolution of the generated image
-        'model': 'image-alpha-001' # Model of the AI service we are using
     }
 
     # Set up a request to the given endpoint
     response = requests.post(
         'https://api.openai.com/v1/images/generations',
-        headers=headers, # Specify headers which we made earelier
+        headers=headers, # Specify headers which we made earlier
         json = data,
-        timeout = 30 # Timeout was manually overidden to avoid premature timeouts as this call can take a while
+        timeout = 60 # Timeout was manually overridden to avoid premature timeouts as this call can take a while
     )
 
     # If the response code is 200 (successful) extract the image URL and return it
     if response.status_code == 200:
         image_url = response.json()['data'][0]['url']
         return image_url
-    # If the response code is not 200 then something must be wrong, tell user and rturn back an error message
+    # If the response code is not 200 then something must be wrong, tell user and return back an error message
     else:
         error_message = f'Ooops... something went wrong'
         return error_message
